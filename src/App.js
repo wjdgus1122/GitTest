c9867d984122be6cb80663f3d960925f58479e9d
wjdgus1122 <qodqodantk@gmail.com>
2022-07-25 09:36:04 +0900

    Plug-in setting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import { Min } from "./Min0723";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
 
 function App() {
   return (
-    <>
-      <Min />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
